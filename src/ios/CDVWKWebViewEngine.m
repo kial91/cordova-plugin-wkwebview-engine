94bc35d877529a4b3c9303a6ab25bfe6832df438
kial91 <ky.culbert@hotmail.co.uk>
2018-01-08 15:51:06 +0000

    Added fix for white border (left for status bar) on iOS11
    Fix found: https://github.com/jcesarmobile/cordova-plugin-disable-ios11-statusbar also seen in https://github.com/ionic-team/cordova-plugin-ionic-webview/blob/master/src/ios/CDVWKWebViewEngine.m

--- src/ios/CDVWKWebViewEngine.m
+++ src/ios/CDVWKWebViewEngine.m
@@ -132,6 +132,14 @@
 
     // re-create WKWebView, since we need to update configuration
     WKWebView* wkWebView = [[WKWebView alloc] initWithFrame:self.frame configuration:configuration];
+ 
+    // fixes issue with status bar leaving a white border on iOS11
+    #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+      [wkWebView.scrollView setContentInsetAdjustmentBehavior:UIScrollViewContentInsetAdjustmentNever];
+    }
+    #endif
+ 
     wkWebView.UIDelegate = self.uiDelegate;
     self.engineWebView = wkWebView;
 
